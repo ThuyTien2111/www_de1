87f76891bce951a77c72f0ebc0f994f72dfb0513
ThuyTien2111 <vanbac21112k2@gmail.com>
2023-10-20 19:13:35 +0700

    front end

---
+++ src/main/java/vn/edu/iuh/fit/entity/Appointment.java
@@ -0,0 +1,66 @@
+package vn.edu.iuh.fit;
+
+import jakarta.json.bind.annotation.JsonbDateFormat;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "appointment")
+public class Appointment {
+    @Id
+    @Column(name = "ApptNo")
+    private long apptNo;
+    @Column(name = "Start")
+    @JsonbDateFormat(value = "yyyy-MM-dd")
+    private LocalDateTime apptStart;
+    @Column(name = "End")
+    private LocalDateTime apptEnd;
+
+    public Appointment() {
+    }
+
+    public Appointment(long apptNo, LocalDateTime apptStart, LocalDateTime apptEnd) {
+        this.apptNo = apptNo;
+        this.apptStart = apptStart;
+        this.apptEnd = apptEnd;
+    }
+
+    public long getApptNo() {
+        return apptNo;
+    }
+
+    public void setApptNo(long apptNo) {
+        this.apptNo = apptNo;
+    }
+
+    public LocalDateTime getApptStart() {
+        return apptStart;
+    }
+
+    public void setApptStart(LocalDateTime apptStart) {
+        this.apptStart = apptStart;
+    }
+
+    public LocalDateTime getApptEnd() {
+        return apptEnd;
+    }
+
+    public void setApptEnd(LocalDateTime apptEnd) {
+        this.apptEnd = apptEnd;
+    }
+
+    @Override
+    public String toString() {
+        return "Appointment{" +
+                "apptNo=" + apptNo +
+                ", apptStart=" + apptStart +
+                ", apptEnd=" + apptEnd +
+                '}';
+    }
+}
